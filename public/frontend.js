4a9aea3532d9a89878d534f52e514128ca3ab8c0
Anton Zorin <20819916+tonyzorin@users.noreply.github.com>
2024-11-14 00:04:15 +0200

    compass issue

--- public/frontend.js
+++ public/frontend.js
@@ -959,6 +959,8 @@ function handleOrientation(event) {
         if (window.orientation) {
             direction += window.orientation;
         }
+        // Fix the direction by inverting it (adding 180 degrees)
+        direction = (360 - direction + 180) % 360;
         gazeIndicator.style.transform = `rotate(${direction}deg)`;
     }
 }
